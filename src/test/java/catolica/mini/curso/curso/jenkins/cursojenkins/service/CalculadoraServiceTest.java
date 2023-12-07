a92c72e7e0cf6d583f2d77d71efc07b1adff9be7
Heitor <heitordiastavares3@gmail.com>
2023-12-07 20:16:42 -0300

    creating some classes and implementing some tests

---
+++ src/test/java/catolica/mini/curso/curso/jenkins/cursojenkins/service/CalculadoraServiceTest.java
@@ -0,0 +1,39 @@
+package catolica.mini.curso.curso.jenkins.cursojenkins.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraServiceTest {
+
+    private CalculadoraService calculadoraService;
+
+    @BeforeEach
+    public void init(){
+        calculadoraService = new CalculadoraService();
+
+    }
+
+    @Test
+    public void deveriaRetornarUmaMensagem(){
+        String hello = "Bem vindo ao curso de jenkins";
+        Assertions.assertEquals(hello, calculadoraService.hello());
+
+    }
+
+    @Test
+    public void deveriaSomarDoisValores(){
+        int a = 2;
+        int b = 2;
+        int result = 4;
+        Assertions.assertEquals(result, calculadoraService.somar(a,b));
+    }
+
+    @Test
+    public void deveriaSubtrairDoisValores(){
+        int a = 8;
+        int b = 2;
+        int result = 6;
+        Assertions.assertEquals(result, calculadoraService.subtrair(a,b));
+    }
+}
